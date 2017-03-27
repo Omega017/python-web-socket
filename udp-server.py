37b260438829897c91e49bd08a27c25de9d27555
Jose Valdebenito <pvpj017@gmail.com>
2017-03-27 17:48:42 -0300

    Modified: Added format verification on server

--- udp-server.py
+++ udp-server.py
@@ -1,5 +1,7 @@
 from socket import *
 
+import re
+
 serverPort = 12000
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
@@ -11,8 +13,11 @@ print "The server is ready to receive"
 while 1:
     sum, clientAddress = serverSocket.recvfrom(2048)
 
-    numbers = sum.split('+')
+    if(re.match('(\s|^|$)(\d+\+\d+)(\s|^|$)', sum)):
+        numbers = sum.split('+')
 
-    numbersAdd = str(int(numbers[0]) + int(numbers[1]))
+        numbersAdd = str(int(numbers[0]) + int(numbers[1]))
 
-    serverSocket.sendto(numbersAdd, clientAddress)
+        serverSocket.sendto(numbersAdd, clientAddress)
+    else:
+        serverSocket.sendto("Incorrect format!", clientAddress)
